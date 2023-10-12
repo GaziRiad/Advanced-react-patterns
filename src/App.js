3852f001ca109a253a8033210588219c71c37f60
GaziRiad <ouhibimustapha36@gmail.com>
2023-10-12 22:29:27 +0100

    A look at Higher Order Components (HOC)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { faker } from "@faker-js/faker";
 import "./style.css";
+import withToggles from "./HOC";
 
 const products = Array.from({ length: 20 }, () => {
   return {
@@ -74,6 +75,19 @@ function List({ title, items, render }) {
   );
 }
 
+// LATER: Let's say we got this component from a 3rd-party library, and can't change it. But we still want to add the 2 toggle functionalities to it
+function ProductList({ title, items }) {
+  return (
+    <ul className="list">
+      {items.map((product) => (
+        <ProductItem key={product.productName} product={product} />
+      ))}
+    </ul>
+  );
+}
+
+const ProductListWithToggles = withToggles(ProductList);
+
 export default function App() {
   return (
     <div>
@@ -99,18 +113,12 @@ export default function App() {
             />
           )}
         />
+        <div className="col-2"></div>
+        <ProductListWithToggles
+          title="Product higher order components"
+          items={products}
+        />
       </div>
     </div>
-  );
-}
-
-// LATER: Let's say we got this component from a 3rd-party library, and can't change it. But we still want to add the 2 toggle functionalities to it
-function ProductList({ title, items }) {
-  return (
-    <ul className="list">
-      {items.map((product) => (
-        <ProductItem key={product.productName} product={product} />
-      ))}
-    </ul>
   );
 }
